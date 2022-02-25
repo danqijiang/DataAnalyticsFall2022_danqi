34fc04bd921aee4b7c382638627850eef941c27a
danqijiang <93158164+danqijiang@users.noreply.github.com>
2022-02-24 22:22:34 -0500

    Add files via upload

---
+++ lab1_data.R
@@ -0,0 +1,16 @@
+EPI_data <- read.csv(file.choose(), header = TRUE)
+
+#or
+#EPI_data <- read.xlsx(”<path>/2010EPI_data.xlsx")
+# Note: replace default data frame name – cannot start with numbers!
+View(EPI_data)
+#
+attach(EPI_data) 	# sets the ‘default’ object
+fix(EPI_data) 	# launches a simple data editor
+EPI 			# prints out values EPI_data$EPI
+tf <- is.na(EPI) # records True values if the value is NA
+E <- EPI[!tf] # filters out NA values, new array
+
+#other data
+GRUMP_data <- read.csv(file.choose(), header = TRUE)
+
